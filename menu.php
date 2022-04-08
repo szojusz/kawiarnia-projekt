5cc55eb338f0c4817bffbccb7555bf1fe9e9831e
szojusz <60299154+szojusz@users.noreply.github.com>
2022-04-08 14:57:44 +0200

    start

---
+++ menu.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-sm navbar-dark bg-">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php"><img src="https://img.icons8.com/windows/24/ffffff/home.png"/>Home</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="mynavbar">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="btn btn-success text-white" href="Zamowienia.php">Zarządzanie Zamówieniami</a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn btn-success text-white" href="Towary.php">Zarządzanie Towarem</a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn btn-success text-white" href=".php">Zamówienia</a>    <!-- brak linku -->
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
